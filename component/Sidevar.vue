9d76eca288e71a5bae046746e316673f987a144e
git12nazneen <lipinazneen65@gmail.com>
2024-12-02 19:49:31 +0600

    added updating sidebar

--- component/Sidevar.vue
+++ component/Sidevar.vue
@@ -1,19 +1,4 @@
-<!-- <script setup lang="ts">
-import DefaultLayout from '@/layouts/DefaultLayout.vue'
-</script>
-
-<template>
-  <DefaultLayout>
-    <h1 class="text-2xl font-bold mb-4">Welcome to Your App</h1>
-    <p>This is the main content area. Your page content goes here.</p>
-
-  
-  </DefaultLayout>
-</template>
-
- -->
-
- <script setup lang=ts>
+<script setup lang=ts>
 import {
   Avatar,
   AvatarFallback,
